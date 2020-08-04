62c74b1aaae91c8fe59a3296e313ba9f2f46aed8
umur inan <umurinan@gmail.com>
2020-08-04 08:53:43 -0500

    subscriber5

---
+++ rabbit-subscriber5/src/main/java/miu/edu/rabbitsubscriber5/RabbitSubscriber5Application.java
@@ -0,0 +1,13 @@
+package miu.edu.rabbitsubscriber5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitSubscriber5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitSubscriber5Application.class, args);
+    }
+
+}
---
+++ rabbit-subscriber5/src/test/java/miu/edu/rabbitsubscriber5/RabbitSubscriber5ApplicationTests.java
@@ -0,0 +1,13 @@
+package miu.edu.rabbitsubscriber5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitSubscriber5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
